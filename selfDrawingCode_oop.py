084d8a7557f32ca520db64926952f340420e8598
Kristin Henry <galaxygoo@gmail.com>
2013-08-17 02:42:32 -0700

    improving version using class. Still needs lots of work

--- selfDrawingCode_oop.py
+++ selfDrawingCode_oop.py
@@ -13,86 +13,77 @@ class selfDrawingCode :
 
 	def __init__(self, fontname=None) :
 
-		print "testing class"
-		self.filename = "selfDrawingCode_oop.py" # ToDo: get this name automatically
 		self.fontname = "Anonymous_Pro.ttf"   # change this to a font you have
 
-		print self.filename
-
-		self.imgSizeX = self.imgSizeY = 500 # set default size of image
-		self.border = 10
-
-		backgroundColor = "white"
-		backgroundColorAlpha = "white"
-
-		self.maxX = self.imgSizeX - self.border
-		self.minX = self.border
-		self.maxY = self.imgSizeY -self.border
-		self.minY = self.border
+		
 
 		self.rMax = 60
-		self.dxMax = 0
-		self.dyMax = 0
 		self.buff = 50
 		self.dx = 0
 		self.dy = 0
+		self.dxMax = 0
+		self.dyMax = 0
 
-		self.letters = []
-		self.counts = []
-		self.letterColors = {}
-
-		self.baseColor = [random.randrange(0,255), random.randrange(0,255), random.randrange(0,255), 100]
-
-		self.xy = [(self.maxX - self.minX)/2 + random.randrange(-100, 100), (self.maxY - self.minY)/2 + random.randrange(-100, 100)]
+		
+		imgSize = [500, 500] # set default size of image, will be resized
+		border = 10
+		maxX = imgSize[0] - border
+		maxY = imgSize[1] - border
+		minX = border
+		minY = border
+		self.xy = [(maxX - minX)/2 + random.randrange(-100, 100), (maxY - minY)/2 + random.randrange(-100, 100)]
 		self.angle = 0
 
 		
 		# get code as text data
+		filename = "selfDrawingCode_oop.py" # ToDo: get the name of this file automatically
 		try :
-			self.data = open(self.filename).readlines()
+			data = open(filename).readlines()
 		except :
-			self.data = ""
-			print "Error reading file: ", self.filename
+			data = ""
+			print "Error reading file: ", filename
 
 		# preprocess data
-		self.getLetters()
-		self.dots = self.getDots()
-		self.minmax = self.getDotsMinMax()
+		letters = []
+		counts = []
+		self.getLetters(data, letters, counts)
 
-		# adjust positions and image size
-		self.shiftDots()
-		self.resizeImage()
+		baseColor = [random.randrange(0,255), random.randrange(0,255), random.randrange(0,255), 100]
+		letterColors = self.getLetterColors(letters, counts, baseColor)
+		
+		dots = self.getDots(data, letterColors)
+		self.minmax = self.getDotsMinMax(dots)
+
+		# adjust positions of dots and image size, so that image contains all dots
+		self.shiftDots(dots)
+		self.resizeImage(imgSize)
 
 		# create background  and image to draw into
 		backgroundColor = "white"
 		backgroundColorAlpha = "white"
-		bkg = Image.new("RGB", (self.imgSizeX, self.imgSizeY), backgroundColor)
-		im = Image.new("RGBA", (self.imgSizeX, self.imgSizeY), backgroundColorAlpha)
+		bkg = Image.new("RGB", (imgSize[0], imgSize[1]), backgroundColor)
+		im = Image.new("RGBA", (imgSize[0], imgSize[1]), backgroundColorAlpha)
 		draw = ImageDraw.Draw(im)
 		
 		# Do the drawing
-		self.drawDots(draw)
-		#drawChars(draw)  # if on linux, can uncomment this
+		self.drawDots(draw, dots)
+		#drawChars(draw)  # if on linux, you may uncomment this
 
 		bkg.paste(im, (0,0), im)
 
 		# save image with source file name, but with png suffix
-		bkg.save(self.filename[:-2] + "png")
+		bkg.save(filename[:-2] + "png")
 
 
 
-	def getLetters(self):
-		data = self.data
+	def getLetters(self, data, letters, counts):
 		for line in data:
 			for char in line:
 				if (char != '\n') & (char != '\t'):
-					self.countLetters(char)
+					self.countLetters(char, letters, counts)
+		
 
-
-	def countLetters(self,char):
-		letters = self.letters
-		counts = self.counts
-
+	def countLetters(self, char, letters, counts):
 		if char in letters:
 			i = letters.index(char)
 			counts[i] += 1
@@ -100,11 +91,10 @@ class selfDrawingCode :
 			letters.append(char)
 			counts.append(1)
 
-	def getLetterColors(self):
-		letters = self.letters
-		counts = self.counts
-		letterColors = self.letterColors
-		baseColor = self.baseColor
+
+	def getLetterColors(self, letters, counts, baseColor):
+
+		letterColors = {}
 
 		for char in letters:
 			i = letters.index(char)
@@ -116,6 +106,8 @@ class selfDrawingCode :
 
 			color = [red, grn, blu, baseColor[3]]
 			letterColors[char] = color
+
+		return letterColors
 		
 
 	def getXYfromChar(self, char):
@@ -134,33 +126,25 @@ class selfDrawingCode :
 		return [xy[0],xy[1]]
 
 
-	def getDots(self):
-		self.letterColors
+	def getDots(self, data, letterColors):
+		
 	 	dots = []
-		# pre-process code from text file
-
-
-		for line in self.data:
+		# determin position and color of each character in code from text file
+		for line in data:
 			for char in line:
 				if char == '\n':
 					char = ' '
 				if (char != '\n') & (char != '\t'):
-
 					xy = self.getXYfromChar(char)
-					
-					# ToDo: why is this in this loop. Fix This!
-					self.getLetterColors()
-
-					c = self.letterColors[char]
+					c = letterColors[char]
 					dot = [char, xy, c]
 					dots.append(dot)
 							
 		return dots
 
 
-	def getDotsMinMax(self):
-		dots = self.dots
-
+	def getDotsMinMax(self, dots):
+		
 		xMin = xMax = 500
 		yMin = yMax = 500
 
@@ -177,7 +161,7 @@ class selfDrawingCode :
 		return [xMin, yMin, xMax, yMax]
 
 
-	def shiftDots(self):
+	def shiftDots(self, dots):
 		
 		minmax = self.minmax
 		dx = self.dx
@@ -186,19 +170,19 @@ class selfDrawingCode :
 		dx = -int(minmax[0]) if minmax[0] > 0 else int(math.fabs(minmax[0]))
 		dy = -int(minmax[1]) if minmax[1] > 0 else int(math.fabs(minmax[1]))
 
-		for dot in self.dots:
+		for dot in dots:
 			p = dot[1]
 			p[0] += dx + self.rMax + self.buff
 			p[1] += dy + self.rMax + self.buff	
 
 
-	def resizeImage(self):
-		self.imgSizeX = int(self.minmax[2]) + self.dx + 2*(self.rMax + self.buff) 
-		self.imgSizeY = int(self.minmax[3]) + self.dy + 2*(self.rMax + self.buff)	
+	def resizeImage(self, imgSize):
+		imgSize[0] = int(self.minmax[2]) + self.dx + 2*(self.rMax + self.buff) 
+		imgSize[1] = int(self.minmax[3]) + self.dy + 2*(self.rMax + self.buff)	
 
 
-	def drawDots(self, draw):
-		for dot in self.dots:
+	def drawDots(self, draw, dots):
+		for dot in dots:
 			x = dot[1][0] 
 			y = dot[1][1] 
 			c = dot[2]
